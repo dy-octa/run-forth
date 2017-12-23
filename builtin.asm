c9446cc5b417f6edc45a7f2c10dde9a44165a1d0
dy-octa <mutian.he@mail.utoronto.ca>
2017-12-23 12:12:55 -0500

    builtin words improvement

--- builtin.asm
+++ builtin.asm
@@ -1,8 +1,78 @@
 .dict
-.:
-movb N T -1 0 0
+
+# Stack operations
+dup:
+movb _ T 1 0 1
 jr
 
+.:
+movb N T -1 0 0 # N = N
+jr
+
+swap:
+movb N R 0 1 0 # R.push(N)
+movb _ T -1 0 1 # T = N, pop
+jal r>
+jr
+
+drop:
+jal .
+jr
+
+nip:
+movb _ T -1 0 1 # N=T, pop
+
+rot:
+jal >r
+jal swap
+jal r>
+jal swap
+jr
+
+# Memory operations
+!:
+movb N [T] 0 0 0
+jr
+
+@:
+movb [T] T 1 0 0
+jr
+
+# Address stack operations
+>r:
+movb _ R -1 1 1
+jr
+
+r>:
+movb R T 1 -1 0
+jr
+
+r@:
+movb R T 1 0 0
+jr
+
+# Comparisons
+<: # T<N
+slt N T 1 0 0
+jr
+
+>: # T>N
+slt N T 1 0 1
+jr
+
+>=: # T>=N
+sge N T 1 0 0
+jr
+
+<=: # T<=N
+sge N T 1 0 1
+jr
+
+=: # T==N
+seq N T 1 0 0
+jr
+
+# Arithmetic
 negate:
 not _ T 0 0 1
 jr
@@ -38,40 +108,4 @@ imm 0
 _max_else:
 jal .
 jal .
-jr
-
-dup:
-movb _ T 1 0 1
-jr
-
->r:
-movb _ R -1 1 1
-jr
-
-r>:
-movb R T 1 -1 0
-jr
-
-r@:
-movb R T 1 0 0
-jr
-
-swap:
-movb N R 0 1 0 # R.push(N)
-movb _ T -1 0 1 # T = N, pop
-jal r>
-jr
-
-drop:
-jal .
-jr
-
-nip:
-movb _ T -1 0 1 # N=T, pop
-
-rot:
-jal >r
-jal swap
-jal r>
-jal swap
 jr
